39d50281389e916a2e4e45f3e087125090a8b2a5
Alex <plast.r.alex@gmail.com>
2019-09-02 19:35:33 -0400

    wasnt set before, but its still broken

--- femrApp/entrypoint.sh
+++ femrApp/entrypoint.sh
@@ -10,7 +10,7 @@ echo "ABOUT TO RUN FEMR"
 /femr/target/universal/femr-"$FEMR_APP_VERSION"/bin/femr \
 "-Dconfig.file=/femr/target/universal/femr-$FEMR_APP_VERSION/prod.conf" \
 "-Dlogger.file=/femr/target/universal/femr-$FEMR_APP_VERSION/conf/prod-logger.xml" \
-"-Duser.home=/femr/target/universal/femr-$FEMR_APP_VERSION" 
+"-Duser.home=/femr/target/universal/femr-$FEMR_APP_VERSION" \
 "-Dplay.http.secret.key=$FEMR_APP_SECRET_KEY"
 
 exit 0
